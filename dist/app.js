8a4045cbdd4e67bb2b3aa132fadfd82baf1e8861
Ananda Gharami <86967865+gharamiananda@users.noreply.github.com>
2026-03-24 07:15:52 +0530

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -17,7 +17,7 @@ const app = (0, express_1.default)();
 //parsers
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)({ origin: ['http://localhost:5173', "http://localhost:5174","https://hireflow-portal.netlify.app"], credentials: true }));
+app.use((0, cors_1.default)({ origin: ['http://localhost:5173', "http://localhost:5174","https://hireflow-portal.netlify.app","https://hireflow-dashboard.netlify.app/"], credentials: true }));
 // application routes
 app.use('/api/v1', routes_1.default);
 app.get('/', (req, res) => {
